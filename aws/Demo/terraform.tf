33684e5aaec8b7e35a5f93fb3878f7c8b977cd81
Krishna-2128 <160769906+Krishna-2128@users.noreply.github.com>
2024-11-30 07:36:14 -0500

    Update terraform.tf

--- aws/Demo/terraform.tf
+++ aws/Demo/terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
   backend "s3" {
     bucket         = "851725612549-terraform-states"
     key            = "terraform/terraform.tfstate"
@@ -6,4 +12,3 @@ terraform {
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
-}
