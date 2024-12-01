25b9c9115ca7f87b86470a1d3e69bf39cf6c4b87
Krishna-2128 <160769906+Krishna-2128@users.noreply.github.com>
2024-12-01 01:52:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "example" {
+  ami           = "ami-0cda377a1b884a1bc"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleInstance"
+ }
+}
