259504f09f3fce8c6e27594b9e64aae712e235e4
Krishna-2128 <160769906+Krishna-2128@users.noreply.github.com>
2024-12-01 01:50:59 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "terraform--backend" #Your bucket name
+    key            = "krishna/terraform.tfstate" #your key name
+    region         = "ap-south-1"
+    dynamodb_table = "terraform--backend" #your dynamodb name
+    encrypt        = true
+  }
+}
